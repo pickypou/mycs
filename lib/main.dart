694f476b23abde9813ab412b4e78d729ecb45c5a
NouveauNom <ludo.spysschaert@orange.fr>
2025-01-16 15:33:40 +0100

    import des fichier firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mycs/core/router/router_config.dart';
 import 'package:mycs/ui/theme.dart';
 
 import 'core/di/di.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Configure toutes les dépendances
   configureDependencies();
+
   runApp(const MyApp());
 }
 
